d6373e5ed55e1d3b45857b4412d767f444c12822
jomc-hash <75284852+jomc-hash@users.noreply.github.com>
2021-09-13 13:12:12 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -18,3 +16,12 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+//función para dar formato a los números telefónicos (###) ###-#####
+document.getElementById('telefono').addEventListener('blur', function (e) {
+  console.log('a')
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{3})(\d{3})(\d{4})/);
+  if (x != null) {
+    e.target.value = '(' + x[1] + ') ' + x[2] + '-' + x[3];
+  }
+});
